89acf1e06a09f45e97c190c518e19c59967d6cb7
Jura Prokofev <Smyrke2005@yandex.ru>
2021-01-13 18:22:15 +0300

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -2,8 +2,11 @@ function canvas_setEffect( effect, canvas, value )
 {
 	if( effect == undefined || canvas == undefined ) return;
 	
-	var newCanvas		= document.createElement( "canvas" );
-	var canvasContext	= canvas.getContext('2d');
+	var newCanvas										= document.createElement( "canvas" );
+	var canvasContext									= canvas.getContext('2d');
+	newCanvas.width										= canvas.width;
+	newCanvas.height									= canvas.height;
+	var newContext										= newCanvas.getContext("2d");
 
 	switch( effect ){
 		case "pixelate":
@@ -19,10 +22,39 @@ function canvas_setEffect( effect, canvas, value )
 			var size									= value / 100;
 			newCanvas.width								= canvas.width * size;
 			newCanvas.height							= canvas.height * size;
-			var dctx									= newCanvas.getContext("2d");
 
-			dctx.drawImage( canvas, 0, 0, newCanvas.width, newCanvas.height );
+			newContext.drawImage( canvas, 0, 0, newCanvas.width, newCanvas.height );
 			canvasContext.drawImage( newCanvas, 0, 0, canvas.width, canvas.height );
+		break;
+		case "outline":
+			var myImg = canvasContext.getImageData( 0, 0, canvas.width, canvas.height );
+
+			for( var y = 0; y < canvas.height; y++ ){
+				for( var x = 0; x < canvas.width; x++ ){
+					var offset							= ( y * canvas.width ) + ( x * 4 );
+					var alpha							= myImg.data[ offset + 4 ];
+					if( alpha > 0 ){
+						offset -= 5 * 4;
+						myImg.data[ offset ]			= 0;
+						myImg.data[ offset + 1 ]		= 0;
+						myImg.data[ offset + 2 ]		= 0;
+						myImg.data[ offset + 3 ]		= 255;
+						offset -= 4 * 4;
+						myImg.data[ offset ]			= 0;
+						myImg.data[ offset + 1 ]		= 0;
+						myImg.data[ offset + 2 ]		= 0;
+						myImg.data[ offset + 3 ]		= 255;
+						offset -= 3 * 4;
+						myImg.data[ offset ]			= 0;
+						myImg.data[ offset + 1 ]		= 0;
+						myImg.data[ offset + 2 ]		= 0;
+						myImg.data[ offset + 3 ]		= 255;
+						break;
+					}
+				}
+			}
+
+			newContext.putImageData( myImg, 0, 0 );
 		break;
 		default: break;
 	}
@@ -44,3 +76,33 @@ function canvas_getClippedRegion( image, x, y, width, height )
 
 	return tempCanvas;
 }
+
+function canvas_roundRect( ctx, x, y, width, height, radius = 5, fill = true, stroke = true)
+{
+	if( ctx == undefined ) return;
+
+	if (typeof radius === 'number') {
+		radius = {tl: radius, tr: radius, br: radius, bl: radius};
+	} else {
+		var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+		for (var side in defaultRadius) {
+			radius[side] = radius[side] || defaultRadius[side];
+		}
+	}
+
+	ctx.beginPath();
+	ctx.moveTo(x + radius.tl, y);
+	ctx.lineTo(x + width - radius.tr, y);
+	ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+	ctx.lineTo(x + width, y + height - radius.br);
+	ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+	ctx.lineTo(x + radius.bl, y + height);
+	ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+	ctx.lineTo(x, y + radius.tl);
+	ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+	ctx.closePath();
+
+	if( fill ) ctx.fill();
+	if( stroke ) ctx.stroke();
+  }
+  
